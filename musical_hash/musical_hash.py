41be90eb35d051e6fe0bf6964e9c2fe4cfaa07fa
m-yuhas <m-yuhas@qq.com>
2020-02-04 21:24:47 -0800

    Starting work in actual structure

---
+++ musical_hash/musical_hash.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Implements a musical hash in Python.
+
+Convert a hash into a sequence of notes so that you can "visualize" any given
+hash.  Conversely, take a sequence of notes and turn them into the bytes of a
+hash to use during computations.
+"""
+
